311de88317e54737c2ff7cd8475bce48089ffe89
Grant Wehrli <grantwehrli18@gmail.com>
2025-10-15 22:07:25 +0000

    added launch_https.sh to fix the login error when demoing on localhost

---
+++ launch_https.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pip install -r requirements.txt
+
+cd aiplaylist || exit
+
+python manage.py collectstatic
+python manage.py makemigrations
+python manage.py migrate
+python3 manage.py runserver_plus --cert-file cert.crt 0.0.0.0:8000
