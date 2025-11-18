65359370c2c5d8a3f285dff5c76456347582bdc0
Grant Wehrli <grantwehrli18@gmail.com>
2025-11-18 19:35:28 +0000

    some linting

--- launch_https.sh
+++ launch_https.sh
@@ -7,4 +7,4 @@ cd src || exit
 yes yes | python manage.py collectstatic > /dev/null
 python manage.py makemigrations
 python manage.py migrate
-python3 manage.py runserver_plus --cert-file cert.crt 0.0.0.0:8000
+python3 manage.py runserver_plus --cert-file cert.crt 0.0.0.0:8888
