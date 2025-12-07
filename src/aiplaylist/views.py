0422ef68e33d4137ebfd09444f39d89415aa52fa
SamExclamationPoint <126426968+SamExclamationPoint@users.noreply.github.com>
2025-12-07 23:51:23 +0000

    fix linting for settings.py and views.py

--- src/aiplaylist/views.py
+++ src/aiplaylist/views.py
@@ -1,3 +1,4 @@
+"""Views for the main landing page"""
 from django.shortcuts import redirect, render
 from django.views import View
 
@@ -7,7 +8,14 @@ from spotify_auth.session import ensure_valid_spotify_session
 class HomeView(View):
     """Display the main landing page"""
 
+
     def get(self, request):
+        """
+        Docstring for get
+        
+        :param self: Description
+        :param request: Description
+        """
         if ensure_valid_spotify_session(request):
             return redirect('dashboard:dashboard')
 
